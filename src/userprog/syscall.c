1c296567b4d00955c675f5d28377d6995ff4bcc6
Ryan Williams <rpwilliams96@viper.cs.ksu.edu>
2018-02-28 16:06:01 -0600

    Added functions for checking valid pointer and buffer

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,8 +3,11 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 
 static void syscall_handler (struct intr_frame *);
+void check_valid_ptr (const void *ptr);
+void check_valid_buffer(void *buffer, unsigned size);
 
 void
 syscall_init (void) 
@@ -14,36 +17,52 @@ syscall_init (void)
 
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
-{
+{  
   // char* argv[3];
+  /* Ensure user provided pointer is valid/safe */
+  check_valid_ptr((const void *) f->esp);
   printf ("system call!\n");
-  /* Based on what the stack pointer is point to, make an appropriate system call */
+  /* Based on what the system call number the
+   stack pointer is point to, make an appropriate system call */
   switch(*(int *) f->esp) {
+  	/* Halt the operating system. */
   	case SYS_HALT:
   		break;
+  	/* Terminate this process. */
   	case SYS_EXIT:
   		// exit(argv[0]);
   		break;
+  	/* Start another process. */
   	case SYS_EXEC:
   		break;
+  	/* Wait for a child process to die. */
   	case SYS_WAIT:
   		break;
+  	/* Create a file. */
   	case SYS_CREATE:
   		break;
+  	/* Delete a file. */
   	case SYS_REMOVE:
   		break;
+  	/* Open a file. */
   	case SYS_OPEN:
   		break;
+  	/* Obtain a file's size. */
   	case SYS_FILESIZE:
   		break;
+  	/* Read from a file. */
   	case SYS_READ:
   		break;
+  	/* Write to a file. */
   	case SYS_WRITE:
   		break;
+  	/* Change position in a file. */
   	case SYS_SEEK:
   		break;
+  	/* Report current position in a file. */
   	case SYS_TELL:
   		break;
+  	/* Close a file. */
   	case SYS_CLOSE:
   		break;
 
@@ -57,9 +76,13 @@ syscall_handler (struct intr_frame *f UNUSED)
 
 // }
 
+
+/* Terminates the current user program, returning status to the kernel. 
+   If the process's parent waits for it, this is the status that
+   will be returned. A status of 0 indicates success and nonzero 
+   values indicate errors. */
 void exit(int status) {
-	/* Print exit message formatted as required,
-	 with process name and exit status when terminated */
+	/* Print process name and exit status */
 	printf("%s: (%d)\n", thread_current()->name, status);
 	thread_exit();
 }
@@ -107,3 +130,23 @@ void exit(int status) {
 // void close (int fd) {
 
 // }
+
+void check_valid_ptr(const void *ptr) {
+	/* If a pointer is null, is not a user virtual address,
+	 or points to unmapped memory, it is invalid*/
+	if(ptr == NULL || !is_user_vaddr(ptr)) {
+		/* Kill process and free resources */
+		exit(-1);
+	}
+}
+
+void check_valid_buffer(void *buffer, unsigned size) {
+	/* If a pointer is null, is not a user virtual address,
+	 or points to unmapped memory, it is invalid*/
+	char *ptr = (char*) buffer;
+	for(unsigned i = 0; i < size; i++) {
+		check_valid_ptr((const void*) ptr);
+		ptr++;
+	}
+	
+}
